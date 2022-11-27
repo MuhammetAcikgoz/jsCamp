8e9a8a50ac1ee0d20314efbe28175196e75cbdd6
Muhammet AÇIKGÖZ <muhammetacikgoz57@gmail.com>
2022-11-27 23:59:04 +0300

    abc

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ console.log (euroDun)
 //array
 //camelCasing
 let konutKredileri=["Konut Kredisi","Araç Kredisi","Gayrimenkul Kredisi","İhtiyaç Kredisi"]
-console.log("<ul>")
+// console.log("<ul>")
 for (let i=0; i<konutKredileri.length; i++) {
     console.log("<li>" + konutKredileri[i] + "</li>")
     }
-console.log("</ul>")
+// console.log("</ul>")
